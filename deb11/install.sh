d0e9264204550af5fe81e128baceb8aad99c892f
Matt <mucha.mateusz@hotmail.com>
2022-07-22 14:22:39 +0200

    Update install.sh

--- deb11/install.sh
+++ deb11/install.sh
@@ -4,7 +4,67 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Install figlet
-sudo apt-get install figlet -y
+# Install tools
+sudo apt install -y htop neofetch apt-transport-https curl
 
-figlet Prepering debian...
+# xorg display server installation
+sudo apt install -y xorg 
+
+# Python installed for bumblebee-status. PACKAGE INCLUDES build-essential.
+sudo apt install -y python3-pip 
+
+# Network Manager
+sudo apt install -y network-manager-gnome
+
+# Installation for Appearance management
+sudo apt install -y lxappearance 
+
+# Install File Manager mc, ranger
+sudp apt install -y mc ranger
+
+# Network File Tools/System Events
+sudo apt install -y dialog mtools dosfstool avahi-daemon acpi acpid gvfs-backends
+
+sudo systemctl enable avahi-daemon
+sudo systemctl enable acpid
+
+# Terminal (eg. terminator,kitty,xfce4-terminal)
+sudo apt install -y kitty
+
+# Sound packages
+sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+
+# Printing and bluetooth (if needed)
+sudo apt install -y cups
+sudo apt install -y bluez blueman
+
+sudo systemctl enable bluetooth
+sudo systemctl enable cups
+
+# Browser Installation (eg. chromium)
+sudo apt install -y firefox-esr 
+
+# Browser Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+
+# Desktop background browser/handler 
+# feh --bg-fill /path/to/directory 
+# example if you want to use in autostart.sh for i3-gaps
+# sudo apt install -y nitrogen 
+sudo apt install -y feh
+
+# Install i3
+sudo apt install i3
+
+# Install Display Manager Lightdm 
+sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo systemctl enable lightdm
+
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
