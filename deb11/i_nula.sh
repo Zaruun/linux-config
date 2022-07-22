6566cad26eb304b930829bd3a28658abc74c0da1
Matt <mucha.mateusz@hotmail.com>
2022-07-22 15:36:05 +0200

    test nula

--- deb11/i_nula.sh
+++ deb11/i_nula.sh
@@ -5,62 +5,62 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Install tools
-sudo apt install -y tmux htop neofetch apt-transport-https curl
+sudo nula install -y tmux htop neofetch apt-transport-https curl
 
 # xorg display server installation
-sudo apt install -y xorg 
+sudo nula install -y xorg 
 
 # Python installed for bumblebee-status. PACKAGE INCLUDES build-essential.
-sudo apt install -y python3-pip 
+sudo nula install -y python3-pip 
 
 # Network Manager
-sudo apt install -y network-manager-gnome
+sudo nula install -y network-manager-gnome
 
 # Installation for Appearance management
-sudo apt install -y lxappearance 
+sudo nula install -y lxappearance 
 
 # Install File Manager mc, ranger
-sudp apt install -y mc ranger
+sudp nula install -y mc ranger
 
 # Network File Tools/System Events
-sudo apt install -y dialog mtools dosfstool avahi-daemon acpi acpid gvfs-backends
+sudo nula install -y dialog mtools dosfstool avahi-daemon acpi acpid gvfs-backends
 
 sudo systemctl enable avahi-daemon
 sudo systemctl enable acpid
 
 # Terminal (eg. terminator,kitty,xfce4-terminal)
-sudo apt install -y kitty
+sudo nula install -y kitty
 
 # Sound packages
-sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+sudo nula install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
 
 # Printing and bluetooth (if needed)
-sudo apt install -y cups
-sudo apt install -y bluez blueman
+sudo nula install -y cups
+sudo nula install -y bluez blueman
 
 sudo systemctl enable bluetooth
 sudo systemctl enable cups
 
 # Browser Installation (eg. chromium)
-sudo apt install -y firefox-esr 
+sudo nula install -y firefox-esr 
 
 # Browser Brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update
+sudo nula update
 
-sudo apt install -y brave-browser
+sudo nula install -y brave-browser
 
 # Desktop background browser/handler 
 # feh --bg-fill /path/to/directory 
 # example if you want to use in autostart.sh for i3-gaps
-# sudo apt install -y nitrogen 
-sudo apt install -y feh
+# sudo nula install -y nitrogen 
+sudo nula install -y feh
 
 # Install i3
-sudo apt install -y i3
+sudo nula install -y i3
 
 # Create folders in user directory (eg. Documents,Downloads,etc.)
 xdg-user-dirs-update
@@ -75,6 +75,6 @@ sudo make install
 sudo systemctl enable ly
 
 # Install Display Manager Lightdm 
-# sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+# sudo nula install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 # sudo systemctl enable lightdm
 
