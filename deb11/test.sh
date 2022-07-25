899240c1be2d38788267fc3afae6f245711b0407
Matt <mucha.mateusz@hotmail.com>
2022-07-25 11:00:34 +0200

    update

---
+++ deb11/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install i3 xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm chromium pulseaudio alsa-utils pavucontrol terminator
